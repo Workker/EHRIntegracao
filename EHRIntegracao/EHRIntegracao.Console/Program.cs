6bd78c8223afd5dd89148b32716dd9e6baeb1fc0
SammuelGarcia <sammuel_garcia@hotmail.com>
2013-04-01 15:32:19 -0300

    implementacao mult thread

--- EHRIntegracao/EHRIntegracao.Console/Program.cs
+++ EHRIntegracao/EHRIntegracao.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EHRIntegracao.Domain.Factorys;
 using EHRIntegracao.Domain.Services;
 using EHRIntegracao.Domain.Services.DTO;
 
@@ -12,20 +13,51 @@ namespace EHRIntegracao.Console
     {
         static void Main(string[] args)
         {
-             var nome =  "LEA FERNANDES FRANCO                    ".Substring(0,3);
-                
-           // SavePatientsCacheService service = new SavePatientsCacheService();
 
-            //Esse serviço eu insiro no memcache com a chave quintaDOrWorkker
-            //service.Save(Domain.Factorys.DbEnum.QuintaDorWorkker);
+
+            //  SavePatientsCacheService service = new SavePatientsCacheService();
+
+            ////  Esse serviço eu insiro no memcache com a chave quintaDOrWorkker
+            //  service.Save(Domain.Factorys.DbEnum.QuintaDorWorkker);
+
+
+            ObterPacientes(DbEnum.QuintaDor, new PatientDTO() { Name = "LEA" }).ContinueWith(Finalizer);
+            ObterPacientes(DbEnum.QuintaDorWorkker, new PatientDTO() { Name = "LEA" }).ContinueWith(Finalizer);
+            ObterPacientes(DbEnum.CopaDor, new PatientDTO() { Name = "LEA" }).ContinueWith(Finalizer);
+            ObterPacientes(DbEnum.sumario, new PatientDTO() { Name = "LEA" }).ContinueWith(Finalizer);
+            //ObterPacientes(DbEnum.QuintaDorWorkker, new PatientDTO() { Name = "LEA" }).ContinueWith(Finalizer);
+            //ObterPacientes(DbEnum.QuintaDorWorkker, new PatientDTO() { Name = "LEA" }).ContinueWith(Finalizer);
+
+            //var pacientesNovos = ObterPacientes(DbEnum.QuintaDorWorkker, new PatientDTO() { Name = "LEA" }).ContinueWith(Finalizer);
+            //var pacientesNovos = ObterPacientes(DbEnum.QuintaDorWorkker, new PatientDTO() { Name = "LEA" }).ContinueWith(Finalizer);
+            //var pacientesNovos = ObterPacientes(DbEnum.QuintaDorWorkker, new PatientDTO() { Name = "LEA" }).ContinueWith(Finalizer);
+
+
+            System.Console.ReadLine();
+        }
+
+        private static void Finalizer(Task<IList<IPatientDTO>> task)
+        {
+            foreach (var item in task.Result)
+            {
+                System.Console.WriteLine(item.Name);
+            }
+        }
+
+        private static Task<IList<IPatientDTO>> ObterPacientes(DbEnum db, IPatientDTO patient)
+        {
+            return Task<IList<IPatientDTO>>.Factory.StartNew(() => GetPatient(db, patient));
+        }
+
+        private static IList<IPatientDTO> GetPatient(DbEnum nome, IPatientDTO patient)
+        {
 
             GetPatientsService serviceGet = new GetPatientsService();
 
             //Aqui eu recupero com a chave quintaDOrWorkker
-            var result = serviceGet.GetPatientsRedis(Domain.Factorys.DbEnum.QuintaDorWorkker, new PatientDTO { Name = "LEA" });
+            var result = serviceGet.GetPatientsRedis(nome, patient);
 
-            //Mas como vc viu, nao deu certo ;/
-            System.Console.ReadLine();
+            return result;
         }
     }
 }
